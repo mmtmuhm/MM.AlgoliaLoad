79df880c489a6b979ea2c50e6eb8169c95e3041e
Tony Muhm <tmuhm@missionmeasurement.com>
2020-09-28 19:39:52 -0500

    Add project files.

---
+++ MM.AlgoliaLoad/Models/SearchItem.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace MM.AlgoliaLoad.Models
+{
+    public class SearchItem
+    {
+        public int Id { get; set; }
+        public int ObjectId { get; set; }
+        public string ProgramName { get; set; }
+        public string ProgramDescription { get; set; }
+        public int Budget { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string UniversalOutcomeId { get; set; }
+        public int OutcomeId { get; set; }
+        public string OutcomeName { get; set; }
+        public string ImpactAreaName { get; set; }
+        public string Genome { get; set; }
+        public string OutcomeDescription { get; set; }
+        public int AverageAchieved { get; set; }
+        public int MinAchieved { get; set; }
+        public int MaxAchieved { get; set; }
+        public int AverageCPO { get; set; }
+        public int MinCPO { get; set; }
+        public int MaxCPO { get; set; }
+        public int AlgoliaObjectId { get; set; }
+
+        public string V1_10 { get; set; }
+        public string V1_11 { get; set; }
+        public string V1_12 { get; set; }
+        public string V2_10 { get; set; }
+        public string V2_11 { get; set; }
+
+        public JObject AlgoliaItem { get; set; }
+
+
+        public SearchItem() 
+        {
+            
+        
+        }
+
+        public SearchItem(mmx_map_data data)
+        {
+            if (data.ImpactArea != null)
+            {
+                V1_10 = string.Format("{0}", data.ImpactArea);
+                V1_11 = string.Format("{0} > {1}", data.ImpactArea, data.ImpactArea);
+                V1_12 = string.Format("{0} > {1} > {2}", data.ImpactArea, data.Genome, data.OutcomeName);
+            }
+
+            V2_10 = string.Format("{0}", data.State);
+            V2_11 = string.Format("{0} > {1}", data.State, data.City);
+
+            AlgoliaItem = new JObject
+            {
+
+                ["Id"] = data.id,
+                ["ObjectId"] = data.object_id,
+                ["ProgramName"] = data.ProgramName,
+                ["ProgramDescription"] = data.ProgramDescription,
+                ["Budget"] = data.Budget,
+                ["Address"] = data.Address,
+                ["City"] = data.City,
+                ["State"] = data.State,
+                ["ZipCode"] = data.Zip_Code,
+                ["UniversalOutcomeId"] = data.UniversalOutcomeId,
+                ["OutcomeId"] = data.OutcomeId,
+                ["OutcomeName"] = data.OutcomeName,
+                ["ImpactAreaName"] = data.ImpactArea,
+                ["Genome"] = data.Genome,
+                ["OutcomeDescription"] = data.OutcomeDescription,
+                ["AverageAchieved"] = data.AverageAcheived,
+                ["MinAchieved"] = data.MinAcheived,
+                ["MaxAchieved"] = data.MaxAcheived,
+                ["AverageCPO"] = data.AverageCPO,
+                ["MinCPO"] = data.MinCPO,
+                ["MaxCPO"] = data.MaxCPO,
+                ["V1_10"] = V1_10,
+                ["V1_11"] = V1_11,
+                ["V1_12"] = V1_12,
+                ["V2_10"] = V2_10,
+                ["V2_11"] = V2_11
+
+            };
+        }
+    }
+
+}
+
+
+
+//ein varchar(15) DEFAULT NULL,
+// OrgName varchar(75) DEFAULT NULL,
+// ProgramName longtext DEFAULT NULL,
+//  ProgramDescription longtext DEFAULT NULL,
+//  Adjusted bigint(20) DEFAULT NULL,
+//  Outcomes bigint(20) NOT NULL,
+//  CPO bigint(20) NOT NULL,
+//  Description longtext NOT NULL,
+//  Budget bigint(20) DEFAULT NULL,
+//  Address varchar(500) DEFAULT NULL,
+//  City varchar(255) DEFAULT NULL,
+//  State varchar(255) DEFAULT NULL,
+//  Zip_Code varchar(255) DEFAULT NULL,
+//  lat varchar(255) DEFAULT NULL,
+//  lon varchar(255) DEFAULT NULL,
+//  UniversalOutcomeId varchar(255) DEFAULT NULL,
+//  OutcomeId int (11) DEFAULT NULL,
+//   OutcomeName varchar(255) DEFAULT NULL,
+//   ImpactArea varchar(255) DEFAULT NULL,
+//   Genome varchar(255) DEFAULT NULL,
+//   OutcomeDescription text DEFAULT NULL,
+//  AverageAcheived bigint(20) DEFAULT NULL,
+//  MinAcheived bigint(20) DEFAULT NULL,
+//  MaxAcheived bigint(20) DEFAULT NULL,
+//  AverageCPO bigint(20) DEFAULT NULL,
+//  MinCPO bigint(20) DEFAULT NULL,
+//  MaxCPO bigint(20) DEFAULT NULL,
+//  AlgoliaObjectId bigint(20) DEFAULT NULL,
---
+++ MM.AlgoliaLoad/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Algolia.Search.Clients;
+using MM.AlgoliaLoad.Models;
+
+namespace MM.AlgoliaLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetData();
+        }
+
+
+
+
+        public static void GetData()
+        {
+
+            using (mmxEntities db = new mmxEntities())
+            {
+                var data = db.mmx_map_data.ToList();
+
+                
+                foreach (var i in data)
+                {
+                    UpdateData(i.id);
+                }
+            
+            }
+        }
+
+        public static void UpdateData(long id)
+        {
+            using (mmxEntities db = new mmxEntities())
+            {
+                var item = db.mmx_map_data.Find(id);
+
+                int ein = Convert.ToInt32(item.ein);
+
+
+                var outcomeLookupItem = db.ntee_outcomes_lookup.Where(_ => _.EIN == ein).FirstOrDefault();
+
+                if (outcomeLookupItem != null)
+                {
+
+                    var outcome = GetOutcomeInfo((int)outcomeLookupItem.outcomeid);
+
+                    if (outcome != null)
+                    {
+                        item.OutcomeId = outcomeLookupItem.outcomeid;
+
+                        item.UniversalOutcomeId = outcome.universaloutcomeid;
+                        item.OutcomeDescription = outcome.description;
+                        item.OutcomeName = outcome.name;
+                        item.Genome = outcome.genomename;
+                        item.ImpactArea = outcome.impactarea;
+
+                        var benchMarks = GetLatestbenchmarks(item.UniversalOutcomeId);
+
+
+                        if (benchMarks != null)
+                        {
+                            item.AverageCPO = benchMarks.avg_cpo;
+                            item.MinCPO = benchMarks.min_cpo;
+                            item.MaxCPO = benchMarks.max_cpo;
+
+                            item.AverageAcheived = item.Budget / benchMarks.avg_cpo;
+                            item.MinAcheived = item.Budget / benchMarks.min_cpo;
+                            item.MaxAcheived = item.Budget / benchMarks.max_cpo;
+
+                            Console.WriteLine(item.id);
+                        }
+                    }
+                }
+
+                var returnId = LoadAlgolia(item);
+
+                item.AlgoliaObjectId = Convert.ToInt32(returnId);
+
+                Console.WriteLine("Object Id: " + item.object_id + " | Algolia Id: " + item.AlgoliaObjectId);
+
+                db.SaveChanges();
+            }
+        }
+
+
+        public static string LoadAlgolia(mmx_map_data item)
+        {
+            SearchClient client = new SearchClient("VMEXWG7ZKH", "3982201c906d85c7556e7ffd933d7422");
+
+            SearchIndex index = client.InitIndex("990_v3");
+
+
+            var algoliaItem = new SearchItem(item);
+
+            var returnVal = index.SaveObject(algoliaItem.AlgoliaItem, autoGenerateObjectId: true);
+
+            return returnVal.Responses[0].ObjectIDs.FirstOrDefault();
+        }
+
+
+
+
+
+
+        public static v_latestbenchmarks GetLatestbenchmarks(string uoid) 
+        {
+            using (taxonomy_dbEntities db = new taxonomy_dbEntities())
+            {
+
+                var i = db.v_latestbenchmarks.Where(_ => _.universalOutcomeId == uoid).FirstOrDefault();
+
+                return i;
+            }
+        }
+
+        public static v_activeoutcomes GetOutcomeInfo(int outcomeid) 
+        {
+            using (taxonomy_dbEntities db = new taxonomy_dbEntities())
+            {
+                var outcome = db.v_activeoutcomes.Where(_ => _.outcomeid == outcomeid).FirstOrDefault();
+
+                return outcome;
+                
+            }
+        }
+    }
+}
